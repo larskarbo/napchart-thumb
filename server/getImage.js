5890ae7aa14356ed96a793a1dea23bdaa401939f
Lars Karbo <mail@larskarbo.no>
2020-08-01 20:39:39 +0200

    firebase!

--- server/getImage.js
+++ server/getImage.js
@@ -1,23 +1,11 @@
 var Napchart = require("napchart");
-var Parse = require("parse/node");
-// const { createCanvas, registerFont } = require('canvas')
 
-Parse.initialize(
-  "osxjLrTMW7cJ6r6IPOpDYXyuBzBRSzQTaNeza7O6",
-  "rijTlVRNfqPPV2X9MLnRAP1UDzQbz7UTRjfCOaQ6"
-);
-Parse.serverURL = 'https://pg-app-57gagyy9xq3pta5kvgpzs2dh6gv7w5.scalabl.cloud/1/';
-// Parse.initialize("napchart");
-// Parse.serverURL = "http://localhost:1337/1/";
-
-// var Canvas = require("canvas"),
-//   Image = Canvas.Image,
-//   canvas = new Canvas(200, 200),
-//   ctx = canvas.getContext("2d");
+var firebase = require("firebase/app");
 
 const { createCanvas, loadImage } = require("canvas");
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
+  var db = firebase.firestore();
   var chartid = req.query.chartid;
   var width = req.query.width * 1; // * 1 to ensure they are numbers not strings
   var height = req.query.height * 1;
@@ -35,41 +23,29 @@ module.exports = function(req, res) {
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext("2d");
 
-  const Chart = Parse.Object.extend("Chart");
-  const query = new Parse.Query(Chart);
-  query.equalTo("chartid", chartid);
-  query
-    .find()
-    .then(results => {
-      console.log("results: ", results);
-      const chart = results[0];
-      console.log();
-      const data = {
-        id: chartid,
-        metaInfo: {
-          title: chart.get("title"),
-          description: chart.get("description")
-        },
-        ...chart.get("chartData"),
-      };
+  const data = await db
+  .collection('charts')
+  .doc(chartid)
+  .get()
+  .then((snapshot) => {
+    const result = snapshot.data()
+    if (result === undefined) {
+      console.log('Chart with ID ' + chartid + ' not found.')
+      return
+    }
+    return result
+  })
 
-      if (typeof shape != "undefined") {
-        data.shape = shape
-      }
+  if (typeof shape != "undefined") {
+    data.shape = shape
+  }
 
+  var mynapchart = Napchart.init(ctx, data, {
+    interaction: false,
+    font: "Consolas",
+    background: "white",
+    baseFontSize: "noscale:1.5"
+  });
 
-
-      var mynapchart = Napchart.init(ctx, data, {
-        interaction: false,
-        font: "Consolas",
-        background: "white",
-        baseFontSize: "noscale:1.5"
-      });
-
-      canvas.pngStream().pipe(res);
-    })
-    .catch(err => {
-      console.log('err: ', err);
-      return res.status(404).send("404");
-    });
+  canvas.pngStream().pipe(res);
 };
--- server/server.js
+++ server/server.js
@@ -4,39 +4,118 @@ var path = require("path");
 var getImage = require("./getImage");
 
 var bodyParser = require("body-parser");
-var Parse = require("parse/node");
-Parse.initialize(
-  "osxjLrTMW7cJ6r6IPOpDYXyuBzBRSzQTaNeza7O6",
-  "rijTlVRNfqPPV2X9MLnRAP1UDzQbz7UTRjfCOaQ6"
-);
-Parse.serverURL =
-  "https://pg-app-57gagyy9xq3pta5kvgpzs2dh6gv7w5.scalabl.cloud/1/";
+
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+// require("firebase/auth");
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZIH0Vogv07ZWCUMwPn1gaBaF_6rAP_zg",
+  authDomain: "napchart-1abe4.firebaseapp.com",
+  databaseURL: "https://napchart-1abe4.firebaseio.com",
+  projectId: "napchart-1abe4",
+  storageBucket: "napchart-1abe4.appspot.com",
+  messagingSenderId: "747326670843",
+  appId: "1:747326670843:web:39891acdbdf5df1cd8ed5e",
+  measurementId: "G-NP62410MLV",
+};
+firebase.initializeApp(firebaseConfig);
+
 app.use(bodyParser.json());
 app.get("/api/getImage", getImage);
 
-app.post("/api/create", function(req, res) {
+app.post("/api/create", function (req, res) {
+  console.log(req.body.data);
+  var db = firebase.firestore();
   var data = JSON.parse(req.body.data);
+  console.log('data: ', data);
 
-  const results = Parse.Cloud.run("createChart", data).then(response => {
-    res.send(response);
-  });
+  const dataForServer = {
+    ...data.chartData,
+    ...data.metaInfo
+  }
+
+  const generateRandomId = () => {
+    const alphabet = 'abcdefghijklmnopqrstuwxyz0123456789'
+    let id = ''
+    for (var i = 0; i < 5; i++) {
+      id += alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+    }
+    return id
+  }
+  
+  const isIdAlreadyTaken = (id) => {
+    return db
+      .collection('charts')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        return doc.exists
+      })
+  }
+
+  const getUniqueChartId = async () => {
+    let id = generateRandomId()
+    while (await isIdAlreadyTaken(id)) {
+      id = generateRandomId()
+    }
+    return id
+  }
+
+  const chartid = getUniqueChartId().then((chartid) => {
+    console.error('generated unique id')
+    console.error(chartid)
+    return db
+      .collection('charts')
+      .doc(chartid)
+      .set(dataForServer)
+      .then((docRef) => {
+        return chartid
+      })
+
+
+  })
+
+  res.send(chartid)
+  console.log('chartid: ', chartid);
+
+
 });
 
-app.get("/api/get", function(req, res) {
+app.get("/api/get", async function (req, res) {
   var chartid = req.query.chartid;
+  var db = firebase.firestore();
 
-  const Chart = Parse.Object.extend("Chart");
-  const query = new Parse.Query(Chart);
-  query.equalTo("chartid", chartid);
-  query.find().then(results => {
-    console.log("results: ", results);
-    const chart = results[0];
-    res.send(results[0]);
-  });
+  const data = await db
+    .collection("charts")
+    .doc(chartid)
+    .get()
+    .then((snapshot) => {
+      const result = snapshot.data();
+      if (result === undefined) {
+        res.status(404).send("Chart with ID " + chartid + " not found.");
+        return;
+      }
+      // want it to be the same as previous version
+      res.send({
+        chartData: {
+          elements: result.elements,
+          colorTags: result.colorTags,
+          shape: result.shape,
+          lanes: result.lanes,
+          lanesConfig: result.lanesConfig,
+        },
+        chartid: chartid,
+        title: result.title,
+        description: result.description,
+      });
+    });
 });
 
 var port = process.env.PORT || 1771;
 
-app.listen(port, "0.0.0.0", function() {
+app.listen(port, "0.0.0.0", function () {
   console.log(`listening at ${port}`);
 });
