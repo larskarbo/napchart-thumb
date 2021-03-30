271c1e9a825e77bcf00e2700ef5cb0c1296f3ac6
Lars Karbo <mail@larskarbo.no>
2021-03-30 20:26:12 +0200

    hr thumb

--- server/getImage.js
+++ server/getImage.js
@@ -6,8 +6,8 @@ let page = false;
   const browser = await puppeteer.launch({ headless: true });
   const pageHere = await browser.newPage();
   await pageHere.setViewport({
-    width: 1500,
-    height: 800,
+    width: 2500,
+    height: 1500,
     deviceScaleFactor: 2,
   });
   await pageHere.goto(`https://napchart.com/app`);
@@ -15,8 +15,13 @@ let page = false;
   page = pageHere;
 })();
 
+const isDev = process.env.NODE_ENV == 'development'
+const WEB_BASE = isDev ? `http://localhost:8000` : `https://napchart.com`
+console.log('WEB_BASE: ', WEB_BASE);
+
 module.exports = async function (req, res) {
   var chartid = req.query.chartid;
+  var hr = req.query.hr;
 
   if (typeof chartid == "undefined") {
     return res.send("Invalid request");
@@ -26,15 +31,17 @@ module.exports = async function (req, res) {
     return res.send(500);
   }
 
+  // const size = hr ? 2400 : 600
+
   (async () => {
-    await page.goto(`https://napchart.com/${chartid}`);
+    await page.goto(`${WEB_BASE}/random/${chartid}`);
     await page.waitForSelector("canvas"); // wait for the selector to load
     const element = await page.$("canvas"); // declare a variable with an ElementHandle
     await element.screenshot({ path: __dirname + "chart.png" });
 
     // await browser.close();
     sharp(__dirname + "chart.png")
-      .resize(600, 600, {
+      .resize(hr ? 2400 : 600, hr ? 2400 : 600, {
         fit: 'cover',
         // position: 'right top',
       })
