008cc8bc01279cb64ac3d9bea36ff242f821fd16
Lars Karbo <mail@larskarbo.no>
2021-04-09 15:37:53 +0200

    new take

--- server/getImage.js
+++ server/getImage.js
@@ -1,28 +1,11 @@
 const puppeteer = require("puppeteer");
 const sharp = require("sharp");
-const { request } = require("./request");
 
-const isDev = process.env.NODE_ENV == 'development'
-const WEB_BASE = isDev ? `http://localhost:8000` : `https://napchart.com`
-console.log('WEB_BASE: ', WEB_BASE);
+const isDev = process.env.NODE_ENV == "development";
+const WEB_BASE = isDev ? `http://localhost:8000` : `https://napchart.com`;
+console.log("WEB_BASE: ", WEB_BASE);
 
-let page = false;
-let element = false;
-(async () => {
-  const browser = await puppeteer.launch({ headless: isDev ? false : true });
-  const pageHere = await browser.newPage();
-  await pageHere.setViewport({
-    width: 2500,
-    height: 1500,
-    deviceScaleFactor: 2,
-  });
-  await pageHere.goto(`${WEB_BASE}/only-the-chart/uhJZRNUCN`);
-  await pageHere.waitForSelector("canvas"); // wait for the selector to load
-  const elementHere = await pageHere.$("canvas"); // declare a variable with an ElementHandle
-  element = elementHere
-  console.log("Pupp is ready");
-  page = pageHere;
-})();
+let id = 40
 
 module.exports = async function (req, res) {
   var chartid = req.query.chartid;
@@ -32,28 +15,30 @@ module.exports = async function (req, res) {
     return res.send("Invalid request");
   }
 
-  if (!page) {
-    return res.send(500);
-  }
-
-  // const size = hr ? 2400 : 600
-  const data = await request("GET", "/getChart/" + chartid);
-
   (async () => {
-    await page.evaluate((chartData) => {
-      window.napchart.setDataCalcShapeAndDraw(chartData)
-    }, data.chartDocument.chartData);
-    await element.screenshot({ path: __dirname + "chart.png" });
+    const idHere = (id++)%20
+    const browser = await puppeteer.launch({ headless: isDev ? false : true });
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 2500,
+      height: 1500,
+      deviceScaleFactor: 2,
+    });
+    await page.goto(`${WEB_BASE}/only-the-chart/${chartid}`);
+    await page.waitForSelector("canvas"); // wait for the selector to load
+    const element = await page.$("canvas"); // declare a variable with an ElementHandle
 
-    // await browser.close();
-    sharp(__dirname + "chart.png")
+    await element.screenshot({ path: __dirname + `/output/${idHere}.png` });
+
+    await browser.close();
+    sharp(__dirname + `/output/${idHere}.png`)
       .resize(hr ? 2400 : 600, hr ? 2400 : 600, {
-        fit: 'cover',
+        fit: "cover",
         // position: 'right top',
       })
-      .toFile(__dirname + "chartcropped.png")
+      .toFile(__dirname + `/output/${idHere}cropped.png`)
       .then((data) => {
-        res.sendFile(__dirname + "chartcropped.png");
+        res.sendFile(__dirname + `/output/${idHere}cropped.png`);
       });
   })();
 };
